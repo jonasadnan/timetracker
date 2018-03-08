514b96a2e889cf64a2781c91c13dc1447b99f7e3
Haakon Kverneng <haakonkverneng@gmail.com>
2018-03-08 12:04:50 +0100

    Added models and migrations

---
+++ app/Project.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Project extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'projects';
+    protected $dates = ['deleted_at'];
+}
---
+++ app/Report.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Report extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'reports';
+    protected $dates = ['deleted_at'];
+}
