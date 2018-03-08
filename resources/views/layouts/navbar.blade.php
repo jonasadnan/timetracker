571754678c20374934eaea2b31c08f01559d7fde
Haakon Kverneng <haakonkverneng@gmail.com>
2018-03-08 10:26:03 +0100

    Added dropdown menu in navbar for user actions

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,5 +1,5 @@
-<header class="header">
-	<div class="col-container">
+<header class="header navbar" style="padding: 0; overflow: visible;">
+	<div class="container">
 		<h1 class="navbar-brand logo-name">
 			Time Tracker
 		</h1>
@@ -9,21 +9,39 @@
 				<li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
 				<li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
 			</ul>
+
 			@else
 				<ul class="nav">
 					<li class="nav-item tasks"><a class="nav-link" href="task_list.php">Tasks</a></li>
 					<li class="nav-item projects"><a class="nav-link" href="project_list.php">Projects</a></li>
 					<li class="nav-item projects"><a class="nav-link" href="category_list.php">Categories</a></li>
 					<li class="nav-item reports"><a class="nav-link" href="reports.php">Reports</a></li>
+					<li class="nav-item dropdown">
+						<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+							{{ Auth::user()->name }} <span class="caret"></span>
+						</a>
+
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="{{ route('logout') }}"
+							   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+								{{ __('Logout') }}
+							</a>
+
+							<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+								@csrf
+							</form>
+						</div>
+					</li>
 				</ul>
-				@endguest
+			@endguest
 	</div>
 </header>
 
 <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
 	<div class="col-container">
-		<a class="navbar-brand" href="#">
+		<div class="navbar-brand">
 			@yield('title')
-		</a>
+		</div>
 	</div>
 </nav>
