ff17c7b54bf2c8c49935ca16c545d4ab090f185d
Haakon Kverneng <haakonkverneng@gmail.com>
2018-03-09 16:49:57 +0100

    Report filters

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Project;
+use App\Task;
 use Illuminate\Http\Request;
 
 class ReportsController extends Controller
@@ -11,9 +14,30 @@ class ReportsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-		return view('reports.index');
+        $tasks = Task::query()
+            ->join('projects', 'tasks.project_id', '=', 'projects.id')
+            ->join('categories', 'projects.category_id', '=', 'categories.id')
+            ->select('tasks.*', 'categories.name as categoryName', 'projects.title as projectTitle')
+            ->orderBy('projects.title');
+
+        $projects = Project::all();
+        $categories = Category::all();
+
+        if ($request->has('project')) {
+            $tasks->where('tasks.project_id', $request->project)->get();
+        }
+
+        if ($request->has('category')) {
+            $tasks->where('projects.category_id', $request->category)->get();
+        }
+
+        $tasks = $tasks->get();
+
+
+
+		return view('reports.index', compact('tasks', 'projects', 'categories'));
     }
 
     /**
